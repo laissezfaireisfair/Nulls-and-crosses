2c8cca9a252eff760439db04dd4c7afb47956d08
suspendwinlogon <suspendwinlogon@gmail.com>
2017-10-01 13:20:03 +0700

    Update game.h
    Рефакторинг с векторами вместо ручной работы с памятью

--- game.h
+++ game.h
@@ -13,17 +13,21 @@ namespace nullsAndCrosses {
     class Game {
         list<Player> (players);
         Field field;
-        int sizeWinCombo, numPlayers;
+        int sizeWinCombo;
         /// Инициализация информации об игре
 		void initializeInfo();
         /// Перезапуск с очисткой поля и возможностью поменять информацию
 		void restartGame();
+		/// Функция для подсчёта пользовательских точек на одной диагонали
+		int  countDio1WithPoint(const char player, const Point newPoint) const;
+		/// И на другой
+		int  countDio2WithPoint(const char player, const Point newPoint) const;
         /// Проверка, победит ли игрок после постановки новой точки
 		bool isWinOrNo(const Point newPoint, const char playerSymbol) const;
 		/// Проверка на ничью
 		bool isDrawOrNo(const int num_steps, const int size) const;
 		/// Обработка 1 хода игры (от 1 юзера)
-		void stepOfGame(Player playerNow);
+		Point stepOfGame(Player playerNow);
         /// Продолжить игру (без инициализации)
 		void continueGame();
     public:
