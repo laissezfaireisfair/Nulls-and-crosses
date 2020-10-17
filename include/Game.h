f0ca14c911cd3ee63ad4e49a55f28f0e801a7f66
Andrey Golubev <laissezfaireisfair@gmail.com>
2020-10-17 11:52:11 +0700

    Remove game realisation from header

--- include/Game.h
+++ include/Game.h
@@ -1,40 +1,24 @@
 #pragma once
-#include <string>
-#include <iostream>
-#include <list>
 #include "Field.h"
 #include "Dialog.h"
+#include "Screen.h"
 #include "Player.h"
+#include "PlayerManager.h"
 
-using namespace std;
 
 namespace nullsAndCrosses {
-  /// Перечисление обработки ошибок
-  enum errorStatus {noError, warning, fatalError};
-
-	 /// Класс обработки игры
     class Game {
-        list<Player> (players);
-        Field field;
-        int sizeWinCombo;
-        /// Инициализация информации об игре
-		    void initialize_info();
-        /// Перезапуск с очисткой поля и возможностью поменять информацию
-		    void restart_game();
-		    /// Функция для подсчёта пользовательских точек на одной диагонали
-		    int  count_dio_1_with_point(const char playerSymbol, const Point newPoint) const;
-		    /// И на другой
-		    int  count_dio_2_with_point(const char playerSymbol, const Point newPoint) const;
-        /// Проверка, победит ли игрок после постановки новой точки
-		    bool is_win_or_no(const Point newPoint, const char playerSymbol) const;
-		    /// Проверка на ничью
-		    bool is_draw_or_no(const int num_steps, const int fieldSize) const;
-		    /// Обработка 1 хода игры (от 1 юзера)
-		    Point step_of_game(Player playerNow);
-        /// Продолжить игру (без инициализации)
-		    void continue_game();
     public:
-        /// Начать игру (с инициализацией)
-		    void new_game();
+        Game();
+		void run();
+    private:
+        enum class GameStatus {NOt_STARTED, RUNNING, DRAW, WIN};
+        Field m_field;
+        PlayerManager m_playerManager;
+        GameStatus m_status;
+        void new_game_without_init();
+        void new_game_with_init();
+        void step();
+        void end();
     };
 }
