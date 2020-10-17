e48c31d6ca84e44499bc3a37363a7de419dfc5f9
Andrey Golubev <laissezfaireisfair@gmail.com>
2020-10-17 10:32:40 +0700

    Update player class to avoid doing other's logic

--- include/Player.h
+++ include/Player.h
@@ -1,43 +1,14 @@
 #pragma once
 #include <string>
-#include <list>
 
 namespace nullsAndCrosses {
-	/// Класс для хранения информации об игроке
 	class Player {
-		std::string name;
-		char symbol;
 	public:
-		Player() {
-			name = "Jonn Doe";
-			symbol = 'x';
-		}
-		Player(std::string n, char sym) {
-			name = n;
-			symbol = sym;
-		}
-		std::string get_name() { return name; }
-		char get_symbol() { return symbol; }
-	};
-
-	/// Класс операций со списком игроков
-	class OperationsWithListOfPlayers {
-	public:
-		static bool is_name_contains(std::list<Player> players, std::string newName) {
-			if (players.empty())
-				return false;
-			if (players.front().get_name() == newName)
-				return true;
-			players.pop_front();
-			return is_name_contains(players, newName);
-		}
-		static bool is_symbol_contains(std::list<Player> players, char newSymbol) {
-			if (players.empty())
-				return false;
-			if (players.front().get_symbol() == newSymbol)
-				return true;
-			players.pop_front();
-			return is_symbol_contains(players, newSymbol);
-		}
+		Player(std::string const & name, char symbol);
+		std::string const & get_name();
+		char get_symbol();
+	private:
+		std::string m_name;
+		char m_symbol;
 	};
 }
