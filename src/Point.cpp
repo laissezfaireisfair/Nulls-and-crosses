a35293e1342250f3da41a521306b1a7f258cd799
Andrey Golubev <laissezfaireisfair@gmail.com>
2020-10-17 12:02:25 +0700

    Add new point interface implementation

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,22 +1,29 @@
 #include "Point.h"
+
 namespace nullsAndCrosses {
-	Point::Point() {
-		x = y = 0;
+	Point::Point() :
+		m_x(0),
+		m_y(0)
+	{}
+
+	Point::Point(int const x, int const y) :
+		m_x(x),
+		m_y(y)
+	{}
+
+	int const & Point::x() const {
+		return m_x;
 	}
-	Point::Point(const int x1, const int y1) {
-		x = x1;
-		y = y1;
-	}
-	void Point::set_x(const int x1) {
-		x = x1;
-	}
-	void Point::set_y(const int y1) {
-		y = y1;
-	}
-	int Point::get_x() const {
-		return x;
-	}
-	int Point::get_y() const {
-		return y;
-	}
+
+    int const & Point::y() const {
+    	return m_y;
+    }
+
+    void Point::set_x(int const x) {
+    	m_x = x;
+    }
+
+    void Point::set_y(int const y) {
+    	m_y = y;
+    }
 }
