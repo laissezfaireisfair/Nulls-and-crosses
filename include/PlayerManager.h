1566d5197333aeec82f7abe1127f8820e60bd68d
Andrey Golubev <laissezfaireisfair@gmail.com>
2020-10-18 12:26:02 +0700

    Add winner info to player manager

--- include/PlayerManager.h
+++ include/PlayerManager.h
@@ -14,7 +14,14 @@ namespace nullsAndCrosses{
         Player const & player(size_t position) const;
         bool is_name_used(std::string const & name) const;
         bool is_symbol_used(char symbol) const;
+        void set_winner(size_t position);
+        void drop_winner();
+        bool is_winner_exist() const;
+        Player & winner();
+        Player const & winner() const;
     private:
+        size_t m_winnerPosition;
+        bool m_winnerSet;
         std::vector<std::shared_ptr<Player>> m_players;
     };
 }
--- src/PlayerManager.cpp
+++ src/PlayerManager.cpp
@@ -3,35 +3,62 @@
 namespace nullsAndCrosses {
 	PlayerManager::PlayerManager() {
 		m_players = std::vector<std::shared_ptr<Player>>();
+        m_winnerSet = false;
+        m_winnerPosition = 0;
 	}
 
-	void add_player(Player const & player) {
+	void PlayerManager::add_player(Player const & player) {
 		m_players.emplace_back(std::make_shared<Player>(player));
 	}
 
-    size_t get_number_of_players() const {
+    size_t PlayerManager::get_number_of_players() const {
     	return m_players.size();
     }
 
-    Player & player(size_t const position) {
+    Player & PlayerManager::player(size_t const position) {
     	return m_players.at(position);
     }
 
-    Player const & player(size_t const position) const {
+    Player const & PlayerManager::player(size_t const position) const {
     	return m_players.at(position);
     }
 
-    bool is_name_used(std::string const & name) const {
+    bool PlayerManager::is_name_used(std::string const & name) const {
     	for (auto &player : m_players)
     		if (player->get_name() == name)
     			return false;
     	return true;
     }
 
-    bool is_symbol_used(char symbol) const {
+    bool PlayerManager::is_symbol_used(char const symbol) const {
     	for (auto &player : m_players)
     		if (player->get_symbol() == symbol)
     			return false;
     	return true;
+    }
+
+    void PlayerManager::set_winner(size_t const position) {
+        m_winnerSet = true;
+        m_winnerPosition = position;
+    }
+
+    void PlayerManager::drop_winner() {
+        m_winnerSet = false;
+        m_winnerPosition = 0;
+    }
+
+    bool PlayerManager::is_winner_exist() const {
+        return m_winnerSet;
+    }
+
+    Player & PlayerManager::winner() {
+        if (!m_winnerSet)
+            throw std::logic_error("Try to get unexisting winner.");
+        return player(m_winnerPosition);
+    }
+    Player const & PlayerManager::winner() const {
+        if (!m_winnerSet)
+            throw std::logic_error("Try to get unexisting winner.");
+        return player(m_winnerPosition);
     }
 }
